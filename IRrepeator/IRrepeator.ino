77c7814c3c60e25a1f7a5591d54e253e3af41f57
smallbeetw <smallbee.tw@gmail.com>
2014-10-27 14:17:46 +0800

    IRrepeator: add IRrepeator.ino
    The code is for Tennsy 2.0 to create an IR repeartor (Re-transmit).
    It's useful to transfer code to another place.
    
    The main code and infiormation reference from:
    	https://www.pjrc.com/teensy/td_libs_IRremote.html

---
+++ IRrepeator/IRrepeator.ino
@@ -0,0 +1,52 @@
+/*
+ * Sketch of Re-transmit IR code
+ *
+ * Copyright (C) 2014 Smallbee.TW <smallbee.tw@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#include <IRremote.h>
+
+const int RECV_PIN = 3;
+
+IRrecv irrecv(RECV_PIN);	// can using any pin to be receiver on Teensy 2.0
+IRsend irsend;			// the specific transmit pin is 10 pin on Teensy 2.0
+
+decode_results results;
+
+unsigned int rawbuf[RAWBUF];
+
+void setup()
+{
+  //Serial.begin(9600);
+  irrecv.enableIRIn();		// Start the receiver
+  irrecv.blink13(true);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    /* convert volatile unsigned int to unsigned int array.
+     * ignore results.rawbuf[0].
+     */
+    for (int i = 0; i < results.rawlen - 1; i++)
+      rawbuf[i] = results.rawbuf[i + 1] * USECPERTICK;
+
+    /* send raw data */
+    irsend.sendRaw(rawbuf, results.rawlen - 1, 38);
+    
+    /* enable interrupt for receive again */
+    irrecv.enableIRIn();
+    irrecv.resume();			// Waiting to receive the next IR code
+  }
+}
