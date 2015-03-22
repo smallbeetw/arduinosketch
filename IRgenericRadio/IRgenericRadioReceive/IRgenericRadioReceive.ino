e8d1c00b4417e891010d8d8f95c0677980f5cb3e
smallbeetw <smallbee.tw@gmail.com>
2015-03-22 22:30:10 +0800

    IRgenericRadio: Add sketch for transfer Arduino IRlibrary compatible IR code through RF
    This send side sketch receive IR code that compatible with Arduino IRlibrary,
    then transfer by RF to receive side. Receive side sketch received IR code
    through RF then emit correspond code. Those 2 sketchs are base on
    maniacbug's RF24 git: https://github.com/maniacbug/RF24

---
+++ IRgenericRadio/IRgenericRadioReceive/IRgenericRadioReceive.ino
@@ -0,0 +1,117 @@
+/*
+ * Sketch of Transfer Generic IR code
+ *
+ * Copyright (C) 2015 Smallbee.TW <smallbee.tw@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#include <IRremote.h>
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+struct decode_results_rf {
+  int decode_type;  // NEC, SONY, RC5, UNKNOWN
+  union {           // This is used for decoding Panasonic and Sharp data
+    unsigned int panasonicAddress;
+    unsigned int sharpAddress;
+  };
+  unsigned long value; // Decoded value
+  int bits;            // Number of bits in decoded value
+};
+
+/* IR sender config */
+IRsend irsend;
+
+/* RF config */
+/* Set up nRF24L01 radio on SPI bus plus pins 9 & 10 */
+RF24 radio(9,10);
+
+/* Topology */
+/* Radio pipe addresses for the 2 nodes to communicate. */
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+void setup(void)
+{
+  /* Print preamble */
+#ifdef DEBUG
+  Serial.begin(9600);
+  printf_begin();
+  printf("\n\rIRTatungRadioReceive\n\r");
+#endif
+
+  /* Setup and configure RF radio */
+  radio.begin();
+
+  /* optionally, increase the delay between retries & # of retries */
+  radio.setRetries(15,15);
+
+  /* Open pipes to other nodes for communication */
+  /* send IR code through radio to receive node */
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+
+  /* Start listening */
+  radio.startListening(); 
+
+  /* Dump the configuration of the rf unit for debugging */
+#ifdef DEBUG
+  radio.printDetails();
+#endif
+}
+
+void loop(void)
+{
+  /* if received RF data */
+  if (radio.available()) {
+      // Dump the payloads until we've gotten everything
+      decode_results_rf results_rf;
+      bool done = false;
+      
+      while (!done)
+      {
+        // Fetch the payload, and see if this was the last one.
+        done = radio.read(&results_rf, sizeof(decode_results_rf));
+#ifdef DEBUG
+        printf("Got payload %lu...", results_rf.value);
+#endif
+	// Delay just a little bit to let the other unit
+	// make the transition to receiver
+	delay(20);
+      }
+
+      // First, stop listening so we can talk
+      radio.stopListening();
+      
+      /* send IR signature */
+      if (results_rf.decode_type == NEC)
+        irsend.sendNEC(results_rf.value, results_rf.bits);
+      else if (results_rf.decode_type == PANASONIC)
+        irsend.sendPanasonic(results_rf.panasonicAddress, results_rf.value);
+      else if (results_rf.decode_type == SONY) {
+        for (int i=0; i < 3; i++) {
+          irsend.sendSony(results_rf.value, results_rf.bits);
+          delay(100);
+        }
+      }
+
+      // Send the final one back.
+      radio.write(&results_rf.value, sizeof(unsigned long));
+#ifdef DEBUG
+      printf("Sent response.\n\r");
+#endif
+
+      // Now, resume listening so we catch the next packets.
+      radio.startListening();
+    }    
+}
---
+++ IRgenericRadio/IRgenericRadioSend/IRgenericRadioSend.ino
@@ -0,0 +1,142 @@
+/*
+ * Sketch of Transfer Generic IR code
+ *
+ * Copyright (C) 2015 Smallbee.TW <smallbee.tw@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+#include <IRremote.h>
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+//#define DEBUG
+
+struct decode_results_rf {
+  int decode_type;  // NEC, SONY, RC5, UNKNOWN
+  union {           // This is used for decoding Panasonic and Sharp data
+    unsigned int panasonicAddress;
+    unsigned int sharpAddress;
+  };
+  unsigned long value; // Decoded value
+  int bits;            // Number of bits in decoded value
+};
+
+/* IR receiver config */
+const int RECV_PIN = 8;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+/* RF config */
+/* Set up nRF24L01 radio on SPI bus plus pins 9 & 10 */
+RF24 radio(9,10);
+
+/* Topology */
+/* Radio pipe addresses for the 2 nodes to communicate. */
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+void setup(void)
+{
+  /* Print preamble */
+#ifdef DEBUG
+  Serial.begin(9600);
+  printf_begin();
+  printf("\n\rIRTatungRadioSend/\n\r");
+#endif
+
+  /* Setup and configure RF radio */
+  radio.begin();
+
+  /* optionally, increase the delay between retries & # of retries */
+  radio.setRetries(15,15);
+
+  /* Open pipes to other nodes for communication */
+  /* send IR code through radio to receive node */
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  /* Start listening */
+  radio.startListening(); //TODO: listen feedback from receive node
+
+  /* Dump the configuration of the rf unit for debugging */
+#ifdef DEBUG
+  radio.printDetails();
+#endif
+
+  /* setup IR receive pin */
+  irrecv.enableIRIn();		// Start the receiver
+  irrecv.blink13(true);
+}
+
+void loop(void)
+{
+  //check received IR signal
+  if (irrecv.decode(&results)) {
+    decode_results_rf results_rf;
+    bool send_ret;
+    
+#ifdef DEBUG
+    printf("got IR\n");
+#endif
+    
+    /* copy results for sending through RF */
+    results_rf.decode_type = results.decode_type;
+    if (results.decode_type == PANASONIC)
+      results_rf.panasonicAddress = results.panasonicAddress;
+    if (results.decode_type == SHARP)
+      results_rf.sharpAddress = results.sharpAddress;
+    results_rf.value = results.value;
+    results_rf.bits = results.bits;
+      
+    /* Stop listening so we can send IR code through RF */
+    radio.stopListening();
+#ifdef DEBUG
+    printf("Now sending %lu...", results_rf.value);
+#endif
+    send_ret = radio.write(&results_rf, sizeof(decode_results_rf));
+#ifdef DEBUG
+    if (send_ret)
+      printf("sent...");
+    else
+      printf("send failed.\n\r");
+#endif
+    /* Continue listening for response from receive node */
+    radio.startListening();
+
+    /* Wait here until we get a response, or timeout (250ms) */
+    unsigned long started_waiting_at = millis();
+    bool timeout = false;
+    while ( ! radio.available() && ! timeout )
+      if (millis() - started_waiting_at > 200 )
+        timeout = true;
+
+    /* Got response or time out*/
+#ifdef DEBUG
+    if (timeout)
+    {
+      printf("Failed, response timed out.\n\r");
+    }
+    else
+    {
+      // Grab the response, compare, and send to debugging spew
+      unsigned long response_code;
+      radio.read(&response_code, sizeof(unsigned long));     
+      printf("Got response code %lu\n", response_code);
+    }
+#endif
+
+    /* enable interrupt for receive again */
+    irrecv.enableIRIn();
+    irrecv.resume();
+  }
+}
